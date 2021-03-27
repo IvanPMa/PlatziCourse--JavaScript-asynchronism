4dd79554fbdd2645e2cf201cf7affdcff4cba25e
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-27 16:31:37 -0600

    Promises

--- asincronismo/src/Promise/index.js
+++ asincronismo/src/Promise/index.js
@@ -11,3 +11,23 @@ const somethingWillHapen = () =>{
 somethingWillHapen()
     .then(response => console.log(response))
     .catch(err => console.error(err));
+
+
+
+
+const somethingWillHapen2 = () =>{
+    return new Promise((resolve, reject) =>{
+        if(true){
+            setTimeout(() =>{
+                resolve('True');
+            }, 2000);
+        } else{
+            const error = new Error('Whoop! ');
+            reject(error);
+        }
+    });
+}
+
+somethingWillHapen2 ()
+    .then( response => console.log(response))
+    .catch(err => console.error(err));
